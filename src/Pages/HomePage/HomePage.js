5d9b24e6e289bd6ead5962cd2c4ee7b13cf3b76d
Madhur Jain <mjain.sps@gmail.com>
2021-05-15 20:43:19 +0530

    latest

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -7,6 +7,13 @@ import "./homePage.css";
 // Importing Images
 import sShape2 from "../../assets/img/shape/s-shape2.png";
 import AmazingWebDevelopCompany from "../../assets/img/slider/Amazing-web-development-company.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGlobe,
+  faBuilding,
+  faMobile,
+  faCog,
+} from "@fortawesome/free-solid-svg-icons";
 function HomePage({ history }) {
   return (
     <>
@@ -55,11 +62,90 @@ function HomePage({ history }) {
 
         {/* Section -2 */}
         <div className="homePage_section2--wrapper">
-          <div className="section2-content">
-            <div>MY NAME IS MADUR</div>
-            <div>MY NAME IS MADUR</div>
-            <div>MY NAME IS MADUR</div>
-            <div>MY NAME IS MADUR</div>
+          <div className="section-2_features--wrapper">
+            <div className="section2-content">
+              <div className="section2-content__component">
+                <div className="section2-content__component-header">
+                  <div className="content__component-logo">
+                    <FontAwesomeIcon icon={faGlobe} />
+                  </div>
+                  <div className="content__component-heading">
+                    <span>COMPLELLING WEBSITES</span>
+                  </div>
+                </div>
+                <p>
+                  Our Static & Dynamic web pages create a powerful impact about
+                  the brand at first glance. Our in-house Web development team
+                  create an intelligent sitemap that binds the audience to the
+                  webpage. We offer a variety of web template designs to choose
+                  from or you may build your own template from scratch.
+                </p>
+              </div>
+
+              <div className="section2-content__component">
+                <div className="section2-content__component-header">
+                  <div className="content__component-logo">
+                    <FontAwesomeIcon icon={faBuilding} />
+                  </div>
+                  <div className="content__component-heading">
+                    <span>WEB APPLICATION DEVELOPMENT</span>
+                  </div>
+                </div>
+                <p>
+                  Productive, useful, and engaging web solutions for
+                  enterprises, start-ups and variety of users who wish to gain
+                  control over their business by having access to critical real
+                  time data on the go. Our applications are totally customised
+                  and very powerful. They are built using latest technologies
+                  available in the market.
+                </p>
+              </div>
+
+              <div className="section2-content__component">
+                <div className="section2-content__component-header">
+                  <div className="content__component-logo">
+                    <FontAwesomeIcon icon={faMobile} />
+                  </div>
+                  <div className="content__component-heading">
+                    <span>MOBILE APP DEVELOPMENT</span>
+                  </div>
+                </div>
+                <p>
+                  Functional, convenient, and user-friendly mobile apps for both
+                  iOS and Android platforms. Our apps are designed to be helpful
+                  in consumer facing and corporate environments.
+                </p>
+              </div>
+
+              <div className="section2-content__component">
+                <div className="section2-content__component-header">
+                  <div className="content__component-logo">
+                    <FontAwesomeIcon icon={faCog} />
+                  </div>
+                  <div className="content__component-heading">
+                    <span>CUSTOM SOFTWARE SOLUTIONS</span>
+                  </div>
+                </div>
+                <p>
+                  All possible kinds of we based software solutions for
+                  businesses, shops, e-commerce, etc. We help you to grow your
+                  business by determining the core problematic areas and
+                  building a custom solution to cater such issues.
+                </p>
+              </div>
+            </div>
+
+            <div className="section2-coreFeatures">
+              <h3>CORE FEATURES</h3>
+              <h1>LOOK WHAT WE DO</h1>
+              <div className="section2-coreFeatures-separator"></div>
+              <CustomButton
+                type="button"
+                onClick={() => history.push("/services")}
+              >
+                LEARN MORE
+              </CustomButton>
+            </div>
           </div>
         </div>
       </Layout>
