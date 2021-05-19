752efdbdaea8a211b3e86e381ae1e5fae3a06a5f
Madhur Jain <mjain.sps@gmail.com>
2021-05-19 15:19:54 +0530

    latest

--- src/Pages/AboutPage/AboutPage.js
+++ src/Pages/AboutPage/AboutPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ComponentBgHeader from "../../Components/ComponentBGHeader/ComponentBgHeader";
+import Layout from "../../Components/Layout/Layout";
 import "./AboutPage.css";
 //importing BG image
 //importing Various Sections
@@ -9,15 +10,17 @@ import GetInTouch from "../../Components/GetInTouch/GetInTouch";
 import aboutPageBG from "../../assets/img/banner/about-us.jpg";
 function AboutPage() {
   return (
-    <div>
-      <ComponentBgHeader bg={aboutPageBG} title="ABOUT US" />
-      {/* Section1 */}
-      <AboutSection1 />
-      {/* Section2 */}
-      <AboutSection2 />
-      {/* Get In Touch Componetn */}
-      <GetInTouch />
-    </div>
+    <Layout>
+      <div>
+        <ComponentBgHeader bg={aboutPageBG} title="ABOUT US" />
+        {/* Section1 */}
+        <AboutSection1 />
+        {/* Section2 */}
+        <AboutSection2 />
+        {/* Get In Touch Componetn */}
+        <GetInTouch />
+      </div>
+    </Layout>
   );
 }
 
--- src/Pages/ServicesPages/ServicesPage.js
+++ src/Pages/ServicesPages/ServicesPage.js
@@ -9,61 +9,66 @@ import websiteDevelopment from "../../assets/img/service/website-development.jpg
 import enterpriceSolutions from "../../assets/img/service/web-application-development.jpg";
 import mobileApplications from "../../assets/img/service/mobile-applications.png";
 import CustomButton from "../../Components/CustomButton/CustomButton";
+//Importing Layout
+import Layout from "../../Components/Layout/Layout";
 function ServicesPage() {
   return (
-    <div>
-      <ComponentBgHeader title="WHAT WE DO" bg={servicesBanner} />
-      <div className="servicesPage--wrapper">
-        <div className="servicesPage--header">
-          <h2> Services</h2>
-          <h1>LOOK WHAT WE DO</h1>
-        </div>
-        <div className="servicesPage--serviceGrid">
-          <div className="servicesPage--gridItem web">
-            <h4>WEBSITE DEVELOPMENT</h4>
-            <img src={websiteDevelopment} alt="" />
-            <p>
-              We build glorious, elegant, and responsive web pages for variety
-              of clients. Our websites have responsive templates and are
-              designed to run on most web browsers and devices such as Desktops,
-              Laptops, Mobiles and Tablets. We follow mobile first approach in
-              designing the sitemap and creating multimedia content for web
-              pages. We offer static as well as dynamic web sites ranging from
-              information websites, subscription-based websites, news websites,
-              business websites and social networking websites.
-            </p>
-            <CustomButton type="button">READ MORE..</CustomButton>
+    <Layout>
+      <div>
+        <ComponentBgHeader title="WHAT WE DO" bg={servicesBanner} />
+        <div className="servicesPage--wrapper">
+          <div className="servicesPage--header">
+            <h2> Services</h2>
+            <h1>LOOK WHAT WE DO</h1>
           </div>
+          <div className="servicesPage--serviceGrid">
+            <div className="servicesPage--gridItem web">
+              <h4>WEBSITE DEVELOPMENT</h4>
+              <img src={websiteDevelopment} alt="" />
+              <p>
+                We build glorious, elegant, and responsive web pages for variety
+                of clients. Our websites have responsive templates and are
+                designed to run on most web browsers and devices such as
+                Desktops, Laptops, Mobiles and Tablets. We follow mobile first
+                approach in designing the sitemap and creating multimedia
+                content for web pages. We offer static as well as dynamic web
+                sites ranging from information websites, subscription-based
+                websites, news websites, business websites and social networking
+                websites.
+              </p>
+              <CustomButton type="button">READ MORE..</CustomButton>
+            </div>
 
-          <div className="servicesPage--gridItem">
-            <h4>WEB APPLICATIONS AND ENTERPRISE SOLUTIONS</h4>
-            <img src={enterpriceSolutions} alt="" />
-            <p>
-              If you have an innovative idea or a specific business objective,
-              we can develop an application to turn your objective into reality.
-              Our solutions find relevance in almost every industry and business
-              type such as salon, restaurants, café, ecommerce, retail,
-              wholesale, manufacturing, etc.
-            </p>
-            <CustomButton type="button">READ MORE..</CustomButton>
-          </div>
+            <div className="servicesPage--gridItem">
+              <h4>WEB APPLICATIONS AND ENTERPRISE SOLUTIONS</h4>
+              <img src={enterpriceSolutions} alt="" />
+              <p>
+                If you have an innovative idea or a specific business objective,
+                we can develop an application to turn your objective into
+                reality. Our solutions find relevance in almost every industry
+                and business type such as salon, restaurants, café, ecommerce,
+                retail, wholesale, manufacturing, etc.
+              </p>
+              <CustomButton type="button">READ MORE..</CustomButton>
+            </div>
 
-          <div className="servicesPage--gridItem mobile_applications">
-            <h4>MOBILE APPLICATIONS</h4>
-            <img src={mobileApplications} alt="" />
-            <p>
-              We build sleek, powerful, productive and ready to use mobile
-              applications which engages your audience and then pay you back
-              with increased loyalty and real world engagement. You can also
-              integrate your existing web portals into a five start mobile
-              application, enhancing your reach to your prospective audience
-            </p>
-            <CustomButton type="button">READ MORE..</CustomButton>
+            <div className="servicesPage--gridItem mobile_applications">
+              <h4>MOBILE APPLICATIONS</h4>
+              <img src={mobileApplications} alt="" />
+              <p>
+                We build sleek, powerful, productive and ready to use mobile
+                applications which engages your audience and then pay you back
+                with increased loyalty and real world engagement. You can also
+                integrate your existing web portals into a five start mobile
+                application, enhancing your reach to your prospective audience
+              </p>
+              <CustomButton type="button">READ MORE..</CustomButton>
+            </div>
           </div>
         </div>
+        <GetInTouch />
       </div>
-      <GetInTouch />
-    </div>
+    </Layout>
   );
 }
 
