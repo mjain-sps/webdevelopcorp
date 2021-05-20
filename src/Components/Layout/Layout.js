41ac37e6a66f203c259316d2996acd26f5c4dcee
Madhur Jain <madhurjain@Madhurs-Mac-mini.local>
2021-05-20 20:42:30 +0530

    latest

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Header from "../Header/Header";
-import MainContent from "../MainContent/MainContent";
+
 import "./layout.css";
 import Footer from "../Footer/Footer";
 function Layout(props) {
